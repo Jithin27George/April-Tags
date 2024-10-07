d13ce4182aa7a5cc5f2420a1ee76ca729c74391c
Jithin27George <gjithin5241235@gmail.com>
2024-10-07 13:48:12 -0400

    Removed tag16h5 and added new directories

---
+++ tag25h9/AprilTag_Generator(tag25h9).py
@@ -0,0 +1,90 @@
+import numpy as np
+from PIL import Image
+import os
+
+def mask(tag_id):
+    tag_size = 7  # The total size of the tag (7x7 grid)
+    cell_size = 20  # Each cell is scaled up by this factor
+
+    # Creating a 6x6 array with 255 (white) values
+    white_blanks = np.ones((7, 7)) * 255
+
+    code=codeblock[tag_id]
+    # Converting the list to a 4x4 NumPy array
+    array_5x5 = np.array(code).reshape(5,5)
+
+    # Setting the border (first and last row/column) to 0 (black)
+    white_blanks[0, :] = 0
+    white_blanks[-1, :] = 0
+    white_blanks[:, 0] = 0
+    white_blanks[:, -1] = 0
+
+    # Filling in the middle 4x4 section based on the values from array_5x5
+    for i in range(5):  # Loop over rows
+        for n in range(5):  # Loop over columns
+            if array_5x5[i, n] == 0:
+                white_blanks[i + 1, n + 1] = 0  # Set corresponding element to 0
+
+    # Create the image from the array and scale it up
+    img = Image.fromarray(white_blanks).convert('L')  # Ensure it's grayscale
+    img = img.resize((tag_size * cell_size, tag_size * cell_size), resample=Image.NEAREST)
+
+    return array_5x5, white_blanks, img
+
+codeblock ={
+    0: [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
+    1: [1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0],
+    2: [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0],
+    3: [1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1],
+    4: [1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0],
+    5: [0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1],
+    6: [0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0],
+    7: [1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1],
+    8: [1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1],
+    9: [0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    10: [0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1],
+    11: [0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    12: [0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0],
+    13: [1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1],
+    14: [1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0],
+    15: [1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+    16: [1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1],
+    17: [0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+    18: [1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+    19: [0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    20: [0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
+    21: [1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+    22: [0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0],
+    23: [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+    24: [1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0],
+    25: [0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0],
+    26: [0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+    27: [1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+    28: [0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+    29: [0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
+    30: [0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    31: [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    32: [0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1],
+    33: [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0],
+    34: [0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0],
+}
+
+if __name__ == "__main__":
+    
+    for tag_id in range (0,35):
+             
+        # Call the mask function to get the arrays and image
+        array_5x5, white_blanks, img = mask(tag_id)
+        
+        # Printing the 4x4 array
+        print("4x4 Array from List:")
+        print(array_5x5)
+        
+        print("\nWhite Blanks Array (after applying the mask):")
+        print(white_blanks)
+        
+        save_directory = '/home/jithin/Desktop/AprilTag/tag25h9/tag25h9/'
+        os.makedirs(save_directory, exist_ok=True)
+        # Save the image to a file
+        img.save(f'apriltag25h9_{tag_id}.png')
+        print(f"AprilTag {tag_id} saved as 'apriltag25h9_{tag_id}.png'.")
